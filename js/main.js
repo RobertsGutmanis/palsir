584bbc24d1d1e4a219f005812470f7b1d5e531f2
Roberts.Gutmanis <sapins23@gmail.com>
2023-11-17 18:18:34 +0200

    added mobile nav side-bar

--- js/main.js
+++ js/main.js
@@ -7,4 +7,20 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 const hiddenElements = document.querySelectorAll(".hidden");
+const mobileNavBtn = document.querySelector(".btn-mobile-nav")
+const mobileNavMenu = document.querySelector(".mobile-nav-menu")
 hiddenElements.forEach((element) => observer.observe(element));
+
+mobileNavBtn.addEventListener("click", (event)=>{
+  mobileNavBtn.classList.toggle("change");
+  mobileNavMenu.classList.toggle("show-menu")
+  mobileNavBtn.classList.toggle("position-fixed")
+})
+
+window.addEventListener("resize", (event)=>{
+  if(event.currentTarget.innerWidth > 850){
+    mobileNavBtn.classList.remove("change")
+    mobileNavBtn.classList.remove("position-fixed")
+    mobileNavMenu.classList.remove("show-menu")
+  }
+})
