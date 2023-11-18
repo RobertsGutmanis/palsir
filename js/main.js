c281392e46323cd8a0f1451fac6f4acc7d3904f6
Roberts.Gutmanis <sapins23@gmail.com>
2023-11-18 20:22:47 +0200

    added youtube video, removed contact us button

--- js/main.js
+++ js/main.js
@@ -13,21 +13,21 @@ const body = document.querySelector("body")
 const html = document.querySelector("html")
 hiddenElements.forEach((element) => observer.observe(element));
 
-mobileNavBtn.addEventListener("click", (event)=>{
-  mobileNavBtn.classList.toggle("change");
-  mobileNavMenu.classList.toggle("show-menu")
-  mobileNavBtn.classList.toggle("position-fixed")
-  // body.classList.toggle("stop-scrolling")
-  if(window.innerWidth<=650){
-    // html.classList.toggle("stop-scrolling")
-  }
-})
+// mobileNavBtn.addEventListener("click", (event)=>{
+//   mobileNavBtn.classList.toggle("change");
+//   mobileNavMenu.classList.toggle("show-menu")
+//   mobileNavBtn.classList.toggle("position-fixed")
+//   // body.classList.toggle("stop-scrolling")
+//   if(window.innerWidth<=650){
+//     // html.classList.toggle("stop-scrolling")
+//   }
+// })
 
-window.addEventListener("resize", (event)=>{
-  if(event.currentTarget.innerWidth > 850){
-    mobileNavBtn.classList.remove("change")
-    mobileNavBtn.classList.remove("position-fixed")
-    mobileNavMenu.classList.remove("show-menu")
-    body.classList.remove("stop-scrolling")
-  }
-})
+// window.addEventListener("resize", (event)=>{
+//   if(event.currentTarget.innerWidth > 850){
+//     mobileNavBtn.classList.remove("change")
+//     mobileNavBtn.classList.remove("position-fixed")
+//     mobileNavMenu.classList.remove("show-menu")
+//     body.classList.remove("stop-scrolling")
+//   }
+// })
